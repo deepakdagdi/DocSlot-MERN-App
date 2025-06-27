4176b63e55c3732a70be06904cb3b4b6be3d00a5
deepak <deepakdagdi.12@gmail.com>
2025-06-27 13:58:51 +0530

    navbar added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Navbar from './componets/Navbar';
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer/>
+      <Navbar/>
+         </div>
+
+  )
+}
+
+export default App
