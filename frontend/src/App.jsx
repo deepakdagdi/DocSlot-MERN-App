3147420fa7cb3a2e70d367edd1dd2ad107274ea5
deepak <deepakdagdi.12@gmail.com>
2025-06-27 14:21:31 +0530

    header,footer and Banner added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 
 import Navbar from './componets/Navbar';
+import Footer from './componets/Footer';
+import {ToastContainer,toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Routes } from 'react-router-dom';
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
       <ToastContainer/>
       <Navbar/>
+      <Routes>
+
+      </Routes>
+      <Footer/>
          </div>
 
   )
